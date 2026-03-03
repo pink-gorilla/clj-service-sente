31e95557b6a1e8a50420eacee4397b8267a5623d
awb99 <awb99@gmail.com>
2026-03-03 09:51:07 -0500

    dep bumps

--- demo/src/demo/app.cljs
+++ demo/src/demo/app.cljs
@@ -1,15 +1,9 @@
 (ns demo.app
   (:require
-   ;[frontend.css :refer [css-loader]]
-   ;[frontend.notification :refer [notification-container]]
-   ;[frontend.dialog :refer [modal-container]]
    [webly.spa.env :refer [get-resource-path]]))
 
 (defn wrap-webly [page match]
   [:div
-   ;[modal-container]
-   ;[notification-container]
-   ;[css-loader (get-resource-path)]
    [page match]])
 
 (def routes
